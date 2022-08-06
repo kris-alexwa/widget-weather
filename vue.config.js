8d494c784311fc032b05cbbe81253d0d941843d9
Kristina <kris-alexwa@yandex.ru>
2022-08-06 12:10:27 +0300

    Configure to use as widget

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: config => {
+    config.optimization.splitChunks = false
+    config.output.filename = 'weather-widget.js'
+    config.output.chunkFilename = 'weather-widget.js'
+  },
+  chainWebpack: config => {
+    if(config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: 'weather-widget.css',
+        chunkFilename: 'weather-widget.css'
+      }])
+    }
+  }
 });
